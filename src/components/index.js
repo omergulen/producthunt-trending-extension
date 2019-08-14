0e083e23a0df31c7b9f3f3572c07be368b9df538
omergulen <omergulen@std.iyte.edu.tr>
2019-08-14 15:50:40 +0300

    unrelevant parts deleted

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as EmptyState } from './EmptyState';
 export { default as Footer } from './Footer';
 export { default as InfoItem } from './InfoItem';
-export { default as LanguageSelect } from './LanguageSelect';
 export { default as PeriodSelect } from './PeriodSelect';
 export { default as RepositoriesList } from './RepositoriesList';
 export { default as RepositoryCard } from './RepositoryCard';
